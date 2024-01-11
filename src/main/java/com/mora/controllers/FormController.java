ca954a4f3fe4a265f79f1e5e2dc640ff26ec2c95
Jhordy Alexander Mora Tabares <jhoalmora91@gmail.com>
2024-01-11 19:21:34 +0100

    Feat: adding html for controllers

--- src/main/java/com/mora/controllers/FormController.java
+++ src/main/java/com/mora/controllers/FormController.java
@@ -4,16 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
     @GetMapping("/form")
-    public String form(Model model){
-       return "form";
+    public String form(Model model) {
+        model.addAttribute("titulo", "Formulario Usuarios");
+        return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(Model model,
+            @RequestParam(value = "username", required = true) String username,
+            @RequestParam(value = "password", required = true) String password,
+            @RequestParam(value = "E-Mail") String email) {
+        model.addAttribute("titulo", "Resultado Form");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
         return "resultado";
     }
 }
