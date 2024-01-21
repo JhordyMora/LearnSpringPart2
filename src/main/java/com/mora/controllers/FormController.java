cddd8799927b64917fc543c50f0d823c2dff5554
Jhordy Alexander Mora Tabares <jhoalmora91@gmail.com>
2024-01-21 12:21:46 +0100

    Feat: Instead Requesparam automatic Usuario request

--- src/main/java/com/mora/controllers/FormController.java
+++ src/main/java/com/mora/controllers/FormController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mora.models.domain.Usuario;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class FormController {
@@ -16,23 +17,38 @@ public class FormController {
         return "form";
     }
 
-    @PostMapping("/form")
-    public String procesar(Model model,
-            @RequestParam(value = "username", required = true) String username,
-            @RequestParam(value = "password", required = true) String password,
-            @RequestParam(value = "email") String email) {
+    // Esta es una manera valida pero la siguiente manera tambien funcion
+    // @PostMapping("/form")
+    // public String procesar(Model model,
+    // @RequestParam(value = "username", required = true) String username,
+    // @RequestParam(value = "password", required = true) String password,
+    // @RequestParam(value = "email") String email) {
 
-        Usuario usuario = new Usuario();
-        usuario.setUsername(username);
-        usuario.setPassword(password);
-        usuario.setEmail(email);
+    // Usuario usuario = new Usuario();
+    // usuario.setUsername(username);
+    // usuario.setPassword(password);
+    // usuario.setEmail(email);
+
+    // model.addAttribute("titulo", "Resultado Form");
+    // // model.addAttribute("username", username);
+    // // model.addAttribute("password", password);
+    // // model.addAttribute("email", email);
+    // model.addAttribute("usuario", usuario);
+
+    // return "resultado";
+    // }
+
+    // En esta manera java spring boot puede hacer el RequesParam automaticamente si
+    // los nombres de los atributos en el html son exactamente iguales a los de la
+    // clase que pongamos. En esta caso la clase es Usuario y los atributos de
+    // usuario (username, password, email) son iguales a los id de el formulario,
+    // por eso el request para se hace automatico
+    @PostMapping("/form")
+    public String procesar(Usuario usuario, Model model) {
 
         model.addAttribute("titulo", "Resultado Form");
-        // model.addAttribute("username", username);
-        // model.addAttribute("password", password);
-        // model.addAttribute("email", email);
         model.addAttribute("usuario", usuario);
-
         return "resultado";
     }
+
 }
