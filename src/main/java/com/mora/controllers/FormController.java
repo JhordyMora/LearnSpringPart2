3ea93740fe5c16da76ac4192f86b3a3444ce38d7
Jhordy Alexander Mora Tabares <jhoalmora91@gmail.com>
2024-01-21 11:55:06 +0100

    Feat: added Usuario class

--- src/main/java/com/mora/controllers/FormController.java
+++ src/main/java/com/mora/controllers/FormController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mora.models.domain.Usuario;
+
 @Controller
 public class FormController {
     @GetMapping("/form")
@@ -19,10 +21,17 @@ public class FormController {
             @RequestParam(value = "username", required = true) String username,
             @RequestParam(value = "password", required = true) String password,
             @RequestParam(value = "E-Mail") String email) {
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        usuario.setEmail(email);
+
         model.addAttribute("titulo", "Resultado Form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        // model.addAttribute("username", username);
+        // model.addAttribute("password", password);
+        // model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
 
         return "resultado";
     }
